57283e9f293f84aca98c6edad9bc98062b99033d
gautamabhinav <abhinavrajgautam01gmail.com>
2023-09-17 12:56:24 +0530

    Adding Reducers with Context Api in Todo-List

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,25 @@
 import Todo from '../Todo/Todo';
 import TodoContext from '../../context/TodoContext';
 import { useContext } from 'react';
+import TodoDispatchContext from '../../context/TodoDispatchContext';
 
 function TodoList() {
-    const {list, setList} = useContext(TodoContext);
+    const {list} = useContext(TodoContext);
+    const {dispatch} = useContext(TodoDispatchContext);
 
+    function onFinished(todo, isFinished) {
+        dispatch({type: 'finish_todo', payload: {todo, isFinished: isFinished}})
+    }
+
+
+    function onDelete(todo) {
+        dispatch({type: 'delete_todo', payload: {todo}})
+    }
+
+    function onEdit(todo, todoText) {
+        dispatch({type: 'edit_todo', payload: {todo, todoText}})
+    }
+    
     return (
         <div>
             {list.length > 0 && 
@@ -13,28 +28,9 @@ function TodoList() {
                                 id={todo.id} 
                                 isFinished={todo.finished} 
                                 todoData={todo.todoData} 
-                                changeFinished={(isFinished) => {
-                                    const updatedList = list.map(t => {
-                                        if(t.id == todo.id){
-                                            todo.finished = isFinished;
-                                        }
-                                        return t;
-                                    });
-                                    setList(updatedList);
-                                }}
-                                onDelete={() => {
-                                    const updatedList = list.filter(t => t.id != todo.id)
-                                    setList(updatedList);
-                                }}
-                                onEdit={(todoText) => {
-                                    const updatedList = list.map(t => {
-                                        if(t.id == todo.id){
-                                            todo.todoData = todoText;
-                                        }
-                                        return t;
-                                    });
-                                    setList(updatedList);
-                                }}
+                                changeFinished={(isFinished) => onFinished(todo, isFinished)}
+                                onDelete={() => onDelete(todo)}
+                                onEdit={(todotext) => onEdit(todo, todotext)}
                             />)}
         </div>
     );
