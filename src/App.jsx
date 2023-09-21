c9586631e5a40261db03b7dbc3ed4518a63fffc0
gautamabhinav <abhinavrajgautam01gmail.com>
2023-09-21 21:44:00 +0530

    Adding Redux toolkit in todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import AddTodo from "./components/AddTodo/AddTodo"
 import TodoList from "./components/TodoList/TodoList"
 import {  bindActionCreators } from "redux";
 
-import { addTodo, deleteTodo, editTodo, todoFinished } from "./actions/todoActions";
+
+import  { addTodo, editTodo, deleteTodo, todoFinished} from "./slices/todoSlice";
 
 function App() {
 
